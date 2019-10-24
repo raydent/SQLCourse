32e3e96730f8ad622ecb4cc86eb35cf3f4526cd2
Andrey Gavrilov <43818472+raydent@users.noreply.github.com>
2019-10-24 12:24:30 +0300

    Rename BDZ1.sql to BDZ/BDZ1.sql

--- BDZ/BDZ1.sql
+++ BDZ/BDZ1.sql
@@ -1,42 +1,42 @@
 USE Polynomes
 GO
 
-DECLARE @n INT, @txt nvarchar(100), @id INT -- пункт 2
+DECLARE @n INT, @txt nvarchar(100), @id INT -- ГЇГіГ­ГЄГІ 2
 SET @n = 1
 SET @txt = ' '
 SET @id = 1
 SELECT @txt = @txt + CAST(coeff as nvarchar(5)) + '*x^' + CAST(pow as nvarchar(5)) + ' + '
-FROM Полиномы
+FROM ГЏГ®Г«ГЁГ­Г®Г¬Г»
 WHERE id = @id
 ORDER BY pow desc
 SELECT REVERSE(STUFF(REVERSE(@txt),1,2,''))
 
-SELECT distinct id -- пункт 3
-FROM Полиномы
+SELECT distinct id -- ГЇГіГ­ГЄГІ 3
+FROM ГЏГ®Г«ГЁГ­Г®Г¬Г»
 WHERE coeff = 0 
 
-DECLARE @num INT, @id INT, @txt nvarchar(100) -- пункт 4
+DECLARE @num INT, @id INT, @txt nvarchar(100) -- ГЇГіГ­ГЄГІ 4
 set @txt = ''
 set @num = 5
 set @id = 2
 SELECT @txt = @txt + CAST(coeff * @num as nvarchar(5)) + '*x^' + CAST(pow as nvarchar(5)) + ' + '
-FROM Полиномы
+FROM ГЏГ®Г«ГЁГ­Г®Г¬Г»
 WHERE id = @id
 ORDER BY pow desc
 SELECT REVERSE(STUFF(REVERSE(@txt),1,2,''))
 
-DECLARE @num INT, @id INT, @txt nvarchar(100), @pow INT --пункт 5
+DECLARE @num INT, @id INT, @txt nvarchar(100), @pow INT --ГЇГіГ­ГЄГІ 5
 set @txt = ''
 set @num = 1
 set @id = 5
 set @pow = 3
 SELECT TOP 1(CASE WHEN pow = @pow THEN 'YES' ELSE 'NO' END)
-FROM Полиномы
+FROM ГЏГ®Г«ГЁГ­Г®Г¬Г»
 WHERE coeff != 0 AND id = @id
 ORDER BY pow DESC
 
 
-DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT  --пункт 6 
+DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT  --ГЇГіГ­ГЄГІ 6 
 set @txt = ''
 set @num = 1
 set @id = 1
@@ -50,7 +50,7 @@ FROM
 SELECT ISNULL(id, 0) + ISNULL(id2, 0) as id, ISNULL(pow,0) + ISNULL(pow2, 0) as pow, ISNULL(coeff, 0) + ISNULL(coeff2, 0) as coeff
 FROM 
 (SELECT*
-FROM (SELECT * FROM Полиномы WHERE id = @id) AS T1 FULL JOIN (SELECT coeff as coeff2, id as id2, pow as pow2 FROM Полиномы WHERE id = @id2) AS T2
+FROM (SELECT * FROM ГЏГ®Г«ГЁГ­Г®Г¬Г» WHERE id = @id) AS T1 FULL JOIN (SELECT coeff as coeff2, id as id2, pow as pow2 FROM ГЏГ®Г«ГЁГ­Г®Г¬Г» WHERE id = @id2) AS T2
 ON T1.pow = T2.pow2
 )AS T3
 )AS T4
