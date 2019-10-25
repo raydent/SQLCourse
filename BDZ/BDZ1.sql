22f00787ca531628159fc8ca47ae5079555f8196
Andrey Gavrilov <43818472+raydent@users.noreply.github.com>
2019-10-26 02:36:53 +0300

    12 tasks done

--- BDZ/BDZ1.sql
+++ BDZ/BDZ1.sql
@@ -57,7 +57,6 @@ WHERE coeff != 0
 )AS T5
 SELECT REVERSE(REVERSE(STUFF(REVERSE(@txt),1,2,'')))
 
-SELECT @txt = @txt + CAST(coeff as nvarchar(5)) + '*x^' + CAST((pow1) as nvarchar(5)) + ' + ' 
 
 DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT  --пункт 7
 set @txt = ''
@@ -82,12 +81,92 @@ ORDER BY pow desc
 SELECT REVERSE(STUFF(REVERSE(@txt),1,2,''))
 
 
- DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT  --пункт 7
+DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT, @sum INT  --пункт 8
 set @txt = ''
-set @num = 1
-set @id = 1
+set @num = 2
+set @id = 3
 set @pow = 3
 set @id2 = 2
-(SELECT*
-FROM (SELECT * FROM Полиномы WHERE id = @id) AS T1 CROSS JOIN (SELECT coeff as coeff2, id as id2, pow as pow2 FROM Полиномы WHERE id = @id2) AS T2
-)
+set @sum = 0
+SELECT @sum = @sum + coeff * cast(POWER(@num, pow) as INT)
+FROM Полиномы
+WHERE id = @id
+ORDER BY pow desc
+SELECT @sum
+
+
+DECLARE @num INT, @id INT, @id2 INT, @txt nvarchar(1000), @pow INT, @sum INT  --пункт 9
+set @txt = ''
+set @num = 2
+set @id = 7
+set @pow = 3
+set @id2 = 2
+set @sum = 0
+SELECT (CASE WHEN SUM(pow) = 3 
+AND (select coeff FROM Полиномы WHERE id = @id and pow = 2) * 2 * (select coeff FROM Полиномы where id = @id and pow = 0) = (select coeff FROM Полиномы where id = @id and pow = 1)
+THEN'YES' 
+ELSE 'NO' END)
+FROM
+Полиномы
+WHERE (id = @id)
+
+
+
+DECLARE @id INT, @max INT, @plus INT, @minus INT, @zero INT  --пункт 10
+set @id = 8
+set @zero = 0
+set @minus = 0
+set @plus = 0
+SELECT @plus = @plus + (CASE WHEN coeff > 0 THEN 1 ELSE 0 END), @minus = @minus + (CASE WHEN coeff < 0 THEN 1 ELSE 0 END), @zero = (SELECT max(pow) - count(pow) + 1 FROM Полиномы WHERE id = @id AND coeff != 0) 
+FROM
+(
+SELECT *
+FROM 
+Полиномы
+WHERE (id = @id) AND coeff != 0
+)AS T
+ORDER BY pow DESC
+SELECT @plus as plus, @minus as minus, @zero as zero
+
+DECLARE @id INT -- пункт 11
+set @id = 1
+SELECT (CASE WHEN (SELECT COUNT(coeff) FROM [Полиномы 1] WHERE id = @id AND CEILING(coeff) != FLOOR(coeff)) != 0 THEN 'NO' ELSE 'YES' END)
+
+DECLARE @id INT -- пункт 12
+set @id = 4
+SELECT (CASE WHEN 
+(SELECT MAX(pow) FROM [Полиномы 1] WHERE id = @id and coeff != 0) = 1 
+THEN 
+CAST(ISNULL((SELECT coeff * (-1) FROM [Полиномы 1] WHERE id = @id AND pow = 0), 0) / (SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 1) 
+as varchar(10))
+ELSE 'NO' END)
+
+
+DECLARE @id INT -- пункт 13
+set @id = 3
+SELECT (CASE WHEN 
+(SELECT MAX(pow) FROM [Полиномы 1] WHERE id = @id and coeff != 0) = 2
+THEN 
+SELECT (CASE WHEN
+(POWER(ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 1), 0), 2) - 
+4 * ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 2), 0) 
+* ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 0), 0)) > 0
+THEN
+CAST(
+(SQRT(POWER(ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 1), 0), 2) 
+- 4 * ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 2), 0) 
+* ISNULL((SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 0), 0)) 
+- ISNULL((SELECT coeff FROM [Полиномы 1] WHERE pow = 1), 0)
+/ 2 * (SELECT coeff FROM Полиномы WHERE pow = 2)) as nvarchar(10))
+ELSE
+'No roots'
+END)
+ELSE
+'NO'
+END)
+
+(SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 0)
+(SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 2)
+(SELECT coeff FROM [Полиномы 1] WHERE id = @id AND pow = 2)
+
+SELECT SQRT(2)
