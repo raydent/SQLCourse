515b30bdfb465651bf734cf0644788814268ad8a
Andrey Gavrilov <43818472+raydent@users.noreply.github.com>
2019-12-09 18:10:32 +0300

    Update BDZ2.sql

--- BDZ/BDZ2.sql
+++ BDZ/BDZ2.sql
@@ -1,4 +1,4 @@
---Бдз 2, практическая часть, выполнили Захар Пылайкин И Андрей Гаврилов
+--Бдз 2, практическая часть
 USE BookStore
 Go
 
@@ -25,15 +25,13 @@ GROUP BY t4.Book, Month(Date_of_order)
 ORDER BY t4.Book
 --
 
---пункт 2 --предполагается, что последний месяц - последний месяц, по которому есть записи в базе данных
+--пункт 2
 SELECT Section, 
 CAST(SUM(Qty_ord * Price_RUR) as float) / 
 (
 	SELECT COUNT(DISTINCT Date_of_Order) FROM Orders 
 	WHERE
-	Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-	AND 
-	MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+	DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 ) as 'Средняя стоимость заказанного в день'
 FROM
 (
@@ -53,9 +51,7 @@ FROM
 	Orders_data t2
 	ON t1.ndoc = t2.ndoc
 	WHERE 
-	Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-	AND 
-	MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+	DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 ) t2
 ON t1.Book_ID = t2.Book_ID
 GROUP BY t1.Section
@@ -84,15 +80,11 @@ FROM
 		Orders_data t2
 		ON t1.ndoc = t2.ndoc
 		WHERE 
-		Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-		AND 
-		MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+		DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 	) t2
 	ON t1.Book_ID = t2.Book_ID
 	WHERE 
-	Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-	AND 
-	MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+	DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 	GROUP BY t1.Name, t1.Surname
 ) as t1
 INNER JOIN
@@ -116,15 +108,11 @@ INNER JOIN
 		Orders_data t2
 		ON t1.ndoc = t2.ndoc
 		WHERE 
-		Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-		AND 
-		MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+		DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 	) t2
 	ON t1.Book_ID = t2.Book_ID
 	WHERE 
-	Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-	AND 
-	MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+	DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 	GROUP BY t1.Name, t1.Surname
 ) as t2
 ON t1.sum_paid <= t2.sum_paid
@@ -145,9 +133,7 @@ LEFT JOIN
 		Orders_data t2
 		ON t1.ndoc = t2.ndoc
 		WHERE 
-		Year(Date_of_Order) = (SELECT DISTINCT Year(MAX(Date_of_Order)) FROM Orders) 
-		AND 
-		MONTH(Date_of_order) = (SELECT DISTINCT MONTH(MAX(Date_of_Order)) FROM Orders)
+		DATEDIFF(MONTH, Date_of_Order, GETDATE()) <= 1
 ) t2
 ON t1.Cust_ID = t2.Cust_ID
 GROUP BY t1.Customer
@@ -171,7 +157,7 @@ NOT EXISTS
 	WHERE 
 	t3.Book_ID = t1.Book_ID 
 	AND
-	t4.Date_of_Order > '01.10.2019'
+	t4.Date_of_Order > '2019.10.01'
 ) 
 AND t2.Qty_in_Stock != 0
 --
@@ -191,7 +177,7 @@ FROM
 		INNER JOIN
 		Orders t2
 		ON t1.ndoc = t2.ndoc
-		WHERE t2.Date_of_Order < '01.10.19'
+		WHERE t2.Date_of_Order < '2019.10.01'
 	) t2
 	ON t1.Cust_ID = t2.Cust_ID
 ) t1
@@ -201,24 +187,133 @@ ON t1.Book_ID = t2.Book_ID
 WHERE
 t2.Qty_in_Stock > 0
 AND 
-t1.Cust_ID != ALL(SELECT Cust_ID FROM Orders WHERE Date_of_Order > '01.10.2019')
+t1.Cust_ID != ALL(SELECT Cust_ID FROM Orders WHERE Date_of_Order > '2019.10.01')
 --
 
---пункт 7 --некоторые книги, зарезервированные в Orders_data, не зарезервированы в Stock, так и должно быть?
-SELECT Book, cast(Qty_rsrv as float) / ISNULL(NULLIF(Qty_in_Stock, 0), 1) as 'Доля в штуках', reserved_price / ISNULL(NULLIF(Qty_in_Stock * Price_RUR, 0), 1) as 'Доля в стоимости'
-FROM
-Stock t1
+--пункт 7 
+SELECT CAST(SUM(Qty_rsrv) as float) / ISNULL(NULLIF(SUM(Qty_in_Stock), 0), 1) as 'Доля в штуках', CAST(SUM(Qty_rsrv * Price_RUR) as float) / ISNULL(NULLIF(SUM(Qty_in_Stock * Price_RUR), 0), 1) as 'Доля в штуках'
+FROM 
+STOCK t1
 INNER JOIN
-(
-	SELECT t1.Book_ID, sum(t2.Qty_ord * t2.Price_RUR) * (SUM(Qty_ord) - SUM(Qty_out)) / SUM(Qty_ord) as reserved_price, t1.Book, t1.Price_RUR
-	FROM 
-	Books t1
-	LEFT JOIN 
-	Orders_data t2
-	On t1.Book_ID = t2.Book_Id
-	WHERE Qty_out = 0 
-	GROUP BY t1.Book_ID, t1.Book, t1.Price_RUR
-	--HAVING Qty_ord != Qty_out
-) t2
+Books t2
 ON t1.Book_ID = t2.Book_ID
+--
+
+--пункт 8
+SELECT Customer, t1.Sum_RUR, (CASE WHEN t1.Pmnt_RUR != t1.Sum_RUR THEN 'Забрал, не оплатил' ELSE 'Оплатил, не забрал' END) as 'Статус' 
+FROM
+(	
+	SELECT t1.Cust_ID, t2.Customer, t1.Pmnt_RUR, t1.Sum_RUR, t1.ndoc
+	FROM
+	Orders t1
+	INNER JOIN
+	Customers t2
+	ON t1.Cust_ID = t2.Cust_ID
+) t1
+Inner join
+Orders_data t2
+ON t1.ndoc = t2.ndoc
+WHERE (t1.Pmnt_RUR = 0 AND t2.Qty_out = t2.Qty_ord) OR (t1.Pmnt_RUR = t1.Sum_RUR AND t2.Qty_out = 0)
+GROUP BY t1.Customer, t1.Cust_ID, t1.Pmnt_RUR, t1.Sum_RUR
+--
+
+--пункт 9
+DECLARE @BOOK_ID int, @Qty int, @Cust_id int
+SET @BOOK_ID = 3 
+SET @Qty = 100000
+SET @Cust_id = 1
+IF (SELECT TOP 1 Qty_in_Stock from Stock WHERE Book_ID = @BOOK_ID) >= @Qty 
+AND
+(SELECT TOP 1 BALANCE FROM Customers WHERE Cust_ID = @Cust_id) >= (SELECT @Qty * Price_RUR FROM Books WHERE Book_ID = @BOOK_ID) 
+	SELECT 1
+ELSE
+	SELECT 0
+--
+
+--пункт 10
+Declare @ndoc int
+set @ndoc = 1337
+UPDATE Orders
+SET Orders.Sum_RUR = (SELECT TOP 1 SUM(Qty_ord * Price_RUR) FROM Orders_data WHERE ndoc = @ndoc GROUP BY ndoc)
+WHERE ndoc = @ndoc
+--
+
+--пункт 11
+DECLARE @ndoc int
+SET @ndoc = 14321992
+IF EXISTS(SELECT Balance, Sum_RUR FROM Orders t1 INNER JOIN Customers t2 ON t1.Cust_ID = t2.Cust_ID WHERE ndoc = @ndoc AND Balance >= Sum_RUR)
+	SELECT 1
+ELSE
+	SELECT 0
+--
+
+--пункт 12
+DECLARE @ndoc int
+SET @ndoc = 1337
+IF EXISTS(SELECT Balance, Sum_RUR FROM Orders t1 INNER JOIN Customers t2 ON t1.Cust_ID = t2.Cust_ID WHERE ndoc = @ndoc AND Balance >= Sum_RUR AND Pmnt_RUR != Sum_RUR)
+BEGIN
+	UPDATE Orders
+	SET Pmnt_RUR = SUM_RUR
+	WHERE ndoc = @ndoc
+	UPDATE Customers
+	SET Balance = Balance - (SELECT Pmnt_RUR from Orders WHERE ndoc = @ndoc)
+	WHERE Cust_ID = (SELECT TOP 1 t1.Cust_ID from Orders t1 INNER JOIN Customers t2 on t1.Cust_ID = t2.Cust_ID WHERE ndoc = @ndoc)
+END
+--
+
+--пункт 13
+DECLARE @ndoc int
+SET @ndoc = 1338
+UPDATE Stock
+SET Qty_rsrv = Qty_rsrv + Данные.Сумма
+FROM Stock INNER JOIN
+(
+    SELECT ndoc, Book_ID, SUM(Qty_ord) as Сумма
+    FROM Orders_data
+    GROUP BY Book_ID, ndoc
+) Данные ON Stock.Book_ID = Данные.Book_ID
+WHERE ndoc = @ndoc
+--
+
+
+--пункт 14
+DECLARE @ndoc int
+SET @ndoc = 1338
+UPDATE Stock
+SET Qty_in_Stock = Qty_in_Stock - Данные.Сумма, Qty_rsrv = Qty_rsrv - Данные.Сумма
+FROM Stock INNER JOIN
+(
+    SELECT ndoc, Book_ID, SUM(Qty_out) as Сумма
+    FROM Orders_data
+    GROUP BY Book_ID, ndoc
+) Данные ON Stock.Book_ID = Данные.Book_ID
+WHERE ndoc = @ndoc AND Qty_in_Stock > Данные.Сумма AND Qty_rsrv > Данные.Сумма
+--
+
+--пункт 15
+UPDATE Stock
+SET Qty_rsrv = Qty_rsrv - Данные.Сумма
+FROM Stock INNER JOIN
+(
+	SELECT t1.ndoc, t1.Сумма, t1.Book_ID, t2.Pmnt_RUR, t2.Date_of_Order
+	FROM
+	(
+		SELECT ndoc, Book_ID, SUM(Qty_ord - Qty_out) as Сумма
+		FROM Orders_data
+		GROUP BY Book_ID, ndoc
+	) t1
+	INNER JOIN
+	Orders t2
+	ON t1.ndoc = t2.ndoc
+) Данные ON Stock.Book_ID = Данные.Book_ID
+WHERE Pmnt_RUR = 0 AND DATEDIFF(day, Date_Of_order, GETDATE()) = 1 AND Сумма != 0
+UPDATE Orders_data
+SET Qty_ord = 0
+FROM Orders_data t1 INNER JOIN
+(
+	SELECT ndoc, Date_of_Order, Pmnt_RUR
+	FROM Orders
+) t2
+ON t1.ndoc = t2.ndoc 
+WHERE Pmnt_RUR = 0 AND DATEDIFF(day, Date_Of_order, GETDATE()) = 1 AND Qty_ord != Qty_out
 --
